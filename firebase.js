8a2f5303e1665dc40337c520591b53cba0a68a0b
damiansoft7-sketch <damian.soft7@gmail.com>
2026-07-16 15:51:47 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,69 +1,17 @@
-// firebase.js
-
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-
-import { 
-
-getDatabase,
-ref,
-set,
-get,
-remove,
-update
-
-} 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-
-
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-
-
-apiKey:"AIzaSyDqdpAchewxlYmGrhFb5WyJK7U5LP2XsA",
-
-authDomain:"tikfinity-overlay.firebaseapp.com",
-
-databaseURL:"https://tikfinity-overlay-default-rtdb.firebaseio.com",
-
-projectId:"tikfinity-overlay",
-
-storageBucket:"tikfinity-overlay.firebasestorage.app",
-
-messagingSenderId:"943639302610",
-
-appId:"1:943639302610:web:5e747094f2f6151b70380b"
-
+  apiKey: "AIzaSyDqdpAchewxlYmGrhFb5WyJKqbL5UP2XsA",
+  authDomain: "tikfinity-overlay.firebaseapp.com",
+  databaseURL: "https://tikfinity-overlay-default-rtdb.firebaseio.com",
+  projectId: "tikfinity-overlay",
+  storageBucket: "tikfinity-overlay.firebasestorage.app",
+  messagingSenderId: "943639302610",
+  appId: "1:943639302610:web:5e747094f2f6151b70380b"
 };
-
-
-
-
 
 const app = initializeApp(firebaseConfig);
-
-
 const database = getDatabase(app);
 
-
-
-
-
-export {
-
-database,
-
-ref,
-
-set,
-
-get,
-
-remove,
-
-update
-
-};
+export { database, ref, set, get };
